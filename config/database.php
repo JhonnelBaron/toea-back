11a84613be78f2dd7436461399b5d38344f9b745
JhonnelBaron <jhonnelbaron03@gmail.com>
2025-10-22 03:32:26 +0000

    connection database to jd

--- config/database.php
+++ config/database.php
@@ -60,7 +60,27 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-        ],
+	],
+	'mysql_jd' => [
+		'driver' => 'mysql',
+		'url' => env('DB_JD_URL'),
+    'host' => env('DB_JD_HOST', '127.0.0.1'),
+    'port' => env('DB_JD_PORT', '3306'),
+    'database' => env('DB_JD_DATABASE', 'forge'),
+    'username' => env('DB_JD_USERNAME', 'forge'),
+    'password' => env('DB_JD_PASSWORD', ''),
+    'unix_socket' => env('DB_SOCKET', ''),
+    'charset' => env('DB_CHARSET', 'utf8mb4'),
+    'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'strict' => true,
+    'engine' => null,
+    'options' => extension_loaded('pdo_mysql') ? array_filter([
+        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+    ]) : [],
+],
+
 
         'mariadb' => [
             'driver' => 'mariadb',
